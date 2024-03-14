2b5b5a0e8fc2cf0f341d7565a99bfd891427c6b4
AlexJoseQV <125517337+AlexJoseQV@users.noreply.github.com>
2024-03-14 09:53:42 +0100

    Actualización

--- api/src/main/java/es/aulanosa/api/repositories/UsuarioRepository.java
+++ api/src/main/java/es/aulanosa/api/repositories/UsuarioRepository.java
@@ -3,6 +3,10 @@ package es.aulanosa.api.repositories;
 import es.aulanosa.api.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Interfaz para el respositorio de Usuario
+ * Esta interfaz extiende JpaRepository, proporcionando métodos CRUD para la entidad Usuario
+ */
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
 
