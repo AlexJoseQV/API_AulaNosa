f0ff355451e90575558a5b504cb70730cff7b086
RubénCampos <113427376+RubenC33@users.noreply.github.com>
2024-03-14 10:17:24 +0100

    subida

---
+++ api/src/main/java/es/aulanosa/api/models/UsuarioEtiqueta.java
@@ -0,0 +1,40 @@
+package es.aulanosa.api.models;
+
+public class UsuarioEtiqueta {
+
+    private int usuario_id; //identificador de usuario//
+
+    private int etiqueta_id; //identificador de etiqueta//
+
+    public int getUsuario_id() {
+        return usuario_id;
+    }
+
+    public UsuarioEtiqueta(int usuario_id, int etiqueta_id) {
+        this.usuario_id = usuario_id;
+        this.etiqueta_id = etiqueta_id;
+    }
+
+    public UsuarioEtiqueta() {
+    }
+
+    public void setUsuario_id(int usuario_id) {
+        this.usuario_id = usuario_id;
+    }
+
+    public int getEtiqueta_id() {
+        return etiqueta_id;
+    }
+
+    public void setEtiqueta_id(int etiqueta_id) {
+        this.etiqueta_id = etiqueta_id;
+    }
+
+    @Override
+    public String toString() {
+        return "UsuarioEtiqueta{" +
+                "usuario_id=" + usuario_id +
+                ", etiqueta_id=" + etiqueta_id +
+                '}';
+    }
+}
---
+++ api/src/main/java/es/aulanosa/api/repositories/UsuarioEtiquetaRepository.java
@@ -0,0 +1,10 @@
+package es.aulanosa.api.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/** Interfaz para el repositorio de UsuarioEtiqueta
+ * * Esta interfaz extiende JpaRepository, proporcionando métodos CRUD para la entidad UsuarioRepository
+ */
+
+public interface UsuarioEtiquetaRepository extends JpaRepository <UsuarioRepository, Integer> {
+
+}
