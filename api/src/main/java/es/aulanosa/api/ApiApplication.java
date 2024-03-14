af3ad76f9f512137b1a57d0a9fd1a3224dd126b3
AlexJoseQV <125517337+AlexJoseQV@users.noreply.github.com>
2024-03-14 10:48:26 +0100

    Actualización

--- api/src/main/java/es/aulanosa/api/ApiApplication.java
+++ api/src/main/java/es/aulanosa/api/ApiApplication.java
@@ -3,9 +3,17 @@ package es.aulanosa.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase principal de la API para la aplicación de egresados de AulaNosa
+ * @author ALEXJOSE
+ */
 @SpringBootApplication
 public class ApiApplication {
 
+	/**
+	 * Punto de entrada principal de la API
+	 * @param args Argumentos de línea de comandos
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
