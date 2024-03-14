fb495cf1f0bda723519829a507b5836705512ac1
RubénCampos <113427376+RubenC33@users.noreply.github.com>
2024-03-14 10:58:26 +0100

    subida

--- api/src/main/java/es/aulanosa/api/ApiApplication.java
+++ api/src/main/java/es/aulanosa/api/ApiApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiApplication {
 
+	/**
+	 * Punto de entrada principal de la API
+	 * @param args Argumentos de línea de comandos
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
