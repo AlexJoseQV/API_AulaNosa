f468e84f8b3eeed69eaaf34f2371272bbca36e0a
Levian17 <martincarballosp@gmail.com>
2024-03-14 12:30:39 +0100

    update

--- api/src/main/java/es/aulanosa/api/repositories/EtiquetaRepository.java
+++ api/src/main/java/es/aulanosa/api/repositories/EtiquetaRepository.java
@@ -3,5 +3,10 @@ package es.aulanosa.api.repositories;
 import es.aulanosa.api.models.Etiqueta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Interfaz para el respositorio de Etiqueta
+ * Esta interfaz extiende JpaRepository, proporcionando métodos CRUD para la entidad Etiqueta
+ * @author MATHIAS
+ */
 public interface EtiquetaRepository extends JpaRepository<Etiqueta, Integer> {
 }
