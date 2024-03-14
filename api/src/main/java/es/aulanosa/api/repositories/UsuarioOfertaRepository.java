3b4973c153027e4d641b48a8c03f9a6e1807f8b1
Levian17 <martincarballosp@gmail.com>
2024-03-14 12:26:28 +0100

    Update

--- api/src/main/java/es/aulanosa/api/repositories/UsuarioOfertaRepository.java
+++ api/src/main/java/es/aulanosa/api/repositories/UsuarioOfertaRepository.java
@@ -4,5 +4,10 @@ import es.aulanosa.api.models.Etiqueta;
 import es.aulanosa.api.models.UsuarioOferta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Interfaz para el respositorio de UsuarioOferta
+ * Esta interfaz extiende JpaRepository, proporcionando métodos CRUD para la entidad UsuarioOferta
+ * @author MARTINCARBALLO
+ */
 public interface UsuarioOfertaRepository extends JpaRepository<UsuarioOferta, Integer> {
 }
