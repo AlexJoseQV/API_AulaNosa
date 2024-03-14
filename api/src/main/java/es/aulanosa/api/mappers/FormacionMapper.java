9b7090bbcd917a0af54d75f94e283c350d8cb19a
Levian17 <martincarballosp@gmail.com>
2024-03-14 13:53:44 +0100

    Update FormacionMapper.java

--- api/src/main/java/es/aulanosa/api/mappers/FormacionMapper.java
+++ api/src/main/java/es/aulanosa/api/mappers/FormacionMapper.java
@@ -6,7 +6,7 @@ import es.aulanosa.api.models.Formacion;
 public class FormacionMapper {
 
     FormacionDTO formacionADTO(Formacion formacion) {
-        return new FormacionDTO();
+        return new FormacionDTO(formacion.getId(), formacion.getTitulo(), formacion.getDescripcion(), formacion.getRequisitos(), formacion.getInicio(), formacion.getFin(), formacion.getFecha(), formacion.getCoste(), formacion.getEstado(), formacion.getImagen());
     };
     Formacion dtoToFormacion(FormacionDTO formacionDTO) {
         return new Formacion();
