add63b21f2143346d62570a3825baf76b807b222
Levian17 <martincarballosp@gmail.com>
2024-03-14 11:25:47 +0100

    Update imagen en la oferta

--- api/src/main/java/es/aulanosa/api/models/Oferta.java
+++ api/src/main/java/es/aulanosa/api/models/Oferta.java
@@ -3,83 +3,94 @@ package es.aulanosa.api.models;
 import jakarta.persistence.*;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 
-    @Table(name = "Ofertas")
-    @Entity
-    public class Oferta {
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Id
+@Table(name = "Ofertas")
+@Entity
+public class Oferta {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
 
-        // Atributos
-        private int id;
-        private int vacantes;
-        private String titulo;
-        private String estado;
-        private String descripcion;
-        private String requisitos; // Mappear desde ArrayList<EtiquetasDTO>
+    // Atributos
+    private int id;
+    private int vacantes;
+    private String titulo;
+    private String estado;
+    private String descripcion;
+    private String requisitos; // Mappear desde ArrayList<EtiquetasDTO>
+    private ArrayList<Byte> imagen;
 
-        // Constructor
-        public Oferta () {}
-
-        // Getters y Setters
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public int getVacantes() {
-            return vacantes;
-        }
-
-        public void setVacantes(int vacantes) {
-            this.vacantes = vacantes;
-        }
-
-        public String getTitulo() {
-            return titulo;
-        }
-
-        public void setTitulo(String titulo) {
-            this.titulo = titulo;
-        }
-
-        public String getEstado() {
-            return estado;
-        }
-
-        public void setEstado(String estado) {
-            this.estado = estado;
-        }
-
-        public String getDescripcion() {
-            return descripcion;
-        }
-
-        public void setDescripcion(String descripcion) {
-            this.descripcion = descripcion;
-        }
-
-        public String getRequisitos() {
-            return requisitos;
-        }
-
-        public void setRequisitos(String requisitos) {
-            this.requisitos = requisitos;
-        }
-
-        // Metodos
-        @Override
-        public String toString() {
-            return "Oferta{" +
-                    "id=" + id +
-                    ", vacantes=" + vacantes +
-                    ", titulo='" + titulo + '\'' +
-                    ", estado='" + estado + '\'' +
-                    ", descripcion='" + descripcion + '\'' +
-                    ", requisitos='" + requisitos + '\'' +
-                    '}';
-        }
+    // Constructor
+    public Oferta() {
     }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVacantes() {
+        return vacantes;
+    }
+
+    public void setVacantes(int vacantes) {
+        this.vacantes = vacantes;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getRequisitos() {
+        return requisitos;
+    }
+
+    public void setRequisitos(String requisitos) {
+        this.requisitos = requisitos;
+    }
+
+    public ArrayList<Byte> getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(ArrayList<Byte> imagen) {
+        this.imagen = imagen;
+    }
+
+    // Metodos
+    @Override
+    public String toString() {
+        return "Oferta{" +
+                "id=" + id +
+                ", vacantes=" + vacantes +
+                ", titulo='" + titulo + '\'' +
+                ", estado='" + estado + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", requisitos='" + requisitos + '\'' +
+                '}';
+    }
+}
--- api/src/test/java/es/aulanosa/api/OfertaRepositoryTests.java
+++ api/src/test/java/es/aulanosa/api/OfertaRepositoryTests.java
@@ -75,5 +75,4 @@ class OfertaRepositoryTests {
 		oferta.setRequisitos("");
 		return oferta;
 	}
-
 }
