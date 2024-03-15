3aa270dd3be7008548c3ee5fae3847ab9c9b48fc
AlexJoseQV <125517337+AlexJoseQV@users.noreply.github.com>
2024-03-15 13:22:32 +0100

    Actualización

---
+++ api/src/main/java/es/aulanosa/api/controllers/OfertaController.java
@@ -0,0 +1,28 @@
+package es.aulanosa.api.controllers;
+
+import es.aulanosa.api.services.OfertaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controlador con los Endpoints respecto a aspectos de las Ofertas
+ * @author ALEXJOSE
+ */
+@RestController
+public class OfertaController {
+
+    @Autowired
+    private OfertaService ofertaService; // Instancia del servicio de Oferta
+
+    /**
+     * Endpoint que permite obtener todas la información de todas las ofertas
+     * @return Se devuelve un listado con la información de todas las ofertas
+     */
+    @GetMapping("/api/ofertas")
+    ResponseEntity<?> obtenerUsuario() {
+
+        return ResponseEntity.ok(ofertaService.listarOfertas());
+    }
+}
