e9ae074f6d922c739877b1957b8f94033c9f1d58
RubénCampos <113427376+RubenC33@users.noreply.github.com>
2024-03-15 12:49:15 +0100

    subida

--- api/src/main/java/es/aulanosa/api/dtos/OfertasDTOSalida.java
+++ api/src/main/java/es/aulanosa/api/dtos/OfertasDTOSalida.java
@@ -1,6 +1,11 @@
 package es.aulanosa.api.dtos;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 public class OfertasDTOSalida {
 
-    
+    private List<OfertasDTO> etiquetas;
+    private List <String> errores;
+    private Timestamp tiempo;
 }
